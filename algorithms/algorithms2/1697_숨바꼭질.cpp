620e342742ab7d0bcb4155e3dec7ab08bfd32c8b
nanenchanga53 <nanenchanga53@gmail.com>
2020-11-12 17:59:37 +0900

    파일위치 정리

---
+++ algorithms/algorithms2/1697_숨바꼭질.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>        //memset
+
+using namespace std;
+
+int n, k;
+
+int main()
+{
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr), cout.tie(nullptr);
+
+        cin >> n >> k;
+
+        return 0;
+}
