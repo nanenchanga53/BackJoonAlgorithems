3b97d778b2dfba892e3a13e5355f736ff3f9a18d
nanenchanga53 <nanenchanga53@gmail.com>
2020-11-26 16:01:01 +0900

    2098_외판원 순회

--- algorithms/algorithms3/2098_외판원 순회.cpp
+++ algorithms/algorithms3/2098_외판원 순회.cpp
@@ -1,20 +1,23 @@
 #include<iostream>
 #include<algorithm>
 #include<cstring>
-#define MAXNUM 16000000
-
 using namespace std;
 
+const int MAXNUM = 1000000000;
 int n;
 int dp[17][1 << 16]; //dp[current][visited] 현재 위치가 current이고 방문했던 도시들이 visited일 때, 부분 경로(집합)최솟값 
 int w[17][17];
 
 int getShortestLoot(int current, int visited)
 {
-	if (visited == (1 << n) - 1)
-		return w[current][1];
-
-	if (dp[current][visited] >= 0)
+	if (visited == (1 << n) - 1) //모두 방문
+	{
+		if (w[current][1] > 0)
+			return w[current][1];
+		else
+			return MAXNUM;
+	}
+	if (dp[current][visited] >= 0) // 방문한 적이 있는 곳
 		return dp[current][visited];
 
 	int ret = MAXNUM;
@@ -22,13 +25,13 @@ int getShortestLoot(int current, int visited)
 	for (int i = 1; i <= n; i++)
 	{
 		int next = i;
-		if ((visited & (1 << (next - 1))) != 0)
+		if ((visited & (1 << (next - 1))) != 0) //방문했던곳
 			continue;
 
-		if (w[current][next] == 0)
+		if (w[current][next] == 0) // 못가는 곳
 			continue;
 
-		int temp = w[current][next] + getShortestLoot(next, visited + (1 << (next - 1)));
+		int temp = w[current][next] + getShortestLoot(next, visited | (1 << (next - 1)));
 		ret = min(ret, temp);
 	}
 
@@ -52,8 +55,7 @@ int main()
 	memset(dp, -1, sizeof(dp));
 
 	int start = 1;
-	cout << getShortestLoot(start, 1);
+	cout << getShortestLoot(start, 1) << '\n';
 
-	system("pause");
 	return 0;
 }
