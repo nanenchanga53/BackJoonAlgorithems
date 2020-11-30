7e35ad0f1934b5f86e3a026d1faf25c4d337d084
nanenchanga53 <nanenchanga53@gmail.com>
2020-11-30 15:50:12 +0900

    Update 17404_RGB거리2.cpp

--- algorithms/algorithms2/17404_RGB거리2.cpp
+++ algorithms/algorithms2/17404_RGB거리2.cpp
@@ -19,9 +19,9 @@ int main()
 
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < 3; j++)
 		{
-			cin >> house[i][j];
+			cin >> house[j][i]; // 같은 색 별로 한줄에 넣어야 되니 [i][j]가 아니다
 		}
 	}
 
@@ -37,11 +37,11 @@ int main()
 		for (int j = 1; j < n; j++)
 		{
 			dp[0][j] = min(dp[1][j - 1], dp[2][j - 1]) + house[0][j];
-			dp[1][j] = min(dp[0][j - 1], dp[2][j - 1]) + house[1][j];
+			dp[1][j] = min(dp[2][j - 1], dp[0][j - 1]) + house[1][j];
 			dp[2][j] = min(dp[0][j - 1], dp[1][j - 1]) + house[2][j];
 		}
 
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < 3; j++)
 		{
 			if (j == i) continue;
 			res = min(res, dp[j][n - 1]);
